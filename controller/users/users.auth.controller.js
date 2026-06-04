65dbf747bb53353aef35e0863122020cc8209691
Chen Vanda <vanda090@gmail.com>
2026-06-04 20:03:07 +0300

    3.Renamed auth controller folder

---
+++ controller/users/users.auth.controller.js
@@ -0,0 +1,59 @@
+import { logUserService, registerUserService, updateUserService, userResetService } from "../../services/users/users.auth.service.js";
+export const logUser = async (req, res) => {
+    try {
+
+        const { userName, password } = req.body;
+        const response = await logUserService(userName, password);
+
+        const { status, ...data } = response
+        res.status(status).json(data)
+
+    } catch (err) {
+
+        res.status(err.status || 500).send(err.message)
+    }
+}
+export const regUser = async (req, res) => {
+    try {
+
+        const userData = req.body;
+        const response = await registerUserService(userData);
+
+        const { status, ...data } = response
+        res.status(status).json(data)
+
+    } catch (err) {
+
+        res.status(err.status || 500).send(err.message)
+    }
+}
+export const updatePassword = async (req, res) => {
+    try {
+
+        const { userID } = req.params;
+        const { updatedPassword } = req.body;
+
+        const response = await updateUserService(userID, updatedPassword);
+
+        const { status, ...data } = response
+        res.status(status).json(data)
+
+    } catch (err) {
+
+        res.status(err.status || 500).send(err.message)
+    }
+}
+export const resetPassword = async (req, res) => {
+    try {
+
+        const { userName } = req.body;
+        const response = await userResetService(userName);
+
+        const { status, ...data } = response
+        res.status(status).json(data)
+
+    } catch (err) {
+
+        res.status(err.status || 500).send(err.message)
+    }
+}
--- services/users/users.auth.service.js
+++ services/users/users.auth.service.js
@@ -1,18 +1,32 @@
 import { hashPassword } from "../../utils/hashPassword.js";
 import { comparePassword } from "../../utils/comparePassword.js";
 
-export const logUserService= async (password)=>{    
-    const hashedPassword= await hashPassword(password);
-    const isValid= await comparePassword(password,hashedPassword)
+export const logUserService = async (userName, password) => {
+    if (!userName) {
+        throw { status: 400, message: "Invalid user name" };
+    }
+    const hashedPassword = await hashPassword(password);
+    const isValid = await comparePassword(password, hashedPassword)
     return isValid;
-}; 
+};
 
-export const registerUserService= async (userData)=>{
-    const {body} = userData.body;
-    if(!body || Object.keys.length==0){
-        throw {status:}
-    }  
-    
-}; 
+export const registerUserService = async (userData) => {
+    if (!userData || Object.keys(userData).length === 0) {
+        throw { status: 400, message: "Invalid data" };
+    }
+    return true;
+};
+export const updateUserService = async (userID, updatedPassword) => {
+    if (!userID || !updatedPassword) {
+        throw { status: 400, message: "Invalid username / password" }
+    }
+};
+export const userResetService = async (userName) => {
+    ;
+    if (!userName) {
+        throw { status: 400, message: "Invalid username" };
+    }
+};
+
 
 
