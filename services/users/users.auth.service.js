0af68bb9e937a3f2d752fee2c9170d52aa797ad0
Chen Vanda <vanda090@gmail.com>
2026-06-04 20:20:03 +0300

    4.Completed auth services structure

--- services/users/users.auth.service.js
+++ services/users/users.auth.service.js
@@ -2,30 +2,32 @@ import { hashPassword } from "../../utils/hashPassword.js";
 import { comparePassword } from "../../utils/comparePassword.js";
 
 export const logUserService = async (userName, password) => {
-    if (!userName) {
-        throw { status: 400, message: "Invalid user name" };
-    }
     const hashedPassword = await hashPassword(password);
-    const isValid = await comparePassword(password, hashedPassword)
-    return isValid;
+    const isValid = await comparePassword(password, hashedPassword);
+     if (!userName || !isValid) {
+        throw { status: 400, message: "Invalid user or password name" };
+    }
+     return {status:200, message:"User has been logged successfully"};
 };
 
 export const registerUserService = async (userData) => {
     if (!userData || Object.keys(userData).length === 0) {
         throw { status: 400, message: "Invalid data" };
     }
-    return true;
+     return {status:200, message:"User has been registered successfully"};
 };
 export const updateUserService = async (userID, updatedPassword) => {
     if (!userID || !updatedPassword) {
-        throw { status: 400, message: "Invalid username / password" }
+      throw { status: 400, message: "Invalid username / password" };
     }
+     return { status: 200, message: "User/Password has been updated successfully" };
 };
 export const userResetService = async (userName) => {
-    ;
+    
     if (!userName) {
         throw { status: 400, message: "Invalid username" };
     }
+    return {status:200, message:"Password has been reseted successfully"};
 };
 
 
