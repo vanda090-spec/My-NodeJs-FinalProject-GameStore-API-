bf5e1b275df775efc68a9390769f12cc9ae42bf8
Chen Vanda <vanda090@gmail.com>
2026-06-10 01:12:50 +0300

    added token validate middleware

--- routes/games/games.js
+++ routes/games/games.js
@@ -1,19 +1,20 @@
 import {Router} from 'express';
 import {getAllGames,getGamesByID,postNewGame,updateGame,deleteGameByID} from "../../controller/games/games.controller.js";
 import { getLiveGames,gamesByCategory,latestNews } from "../../controller/games/games.controller.js"
+import {validateToken} from "../../middleware/validateToken.js";
 export const router=Router();
 
 router.get('/', getAllGames);
 router.get('/gamesInfo/live',getLiveGames);
 router.get('/gamesInfo/category',gamesByCategory);
 router.get('/gamesInfo/news',latestNews);
-router.get('/:gameID', getGamesByID);
+router.get('/:gameID',validateToken,getGamesByID);
 
 router.post('/',postNewGame);
 
-router.put('/:gameID', updateGame);
+router.put('/:gameID',validateToken,updateGame);
 
-router.delete('/:gameID', deleteGameByID);
+router.delete('/:gameID',validateToken,deleteGameByID);
 
 
 export default router;
--- routes/users/users.info.js
+++ routes/users/users.info.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {getAllUsers,getUserById,deleteUserByID} from "../../controller/users/users.info.controller.js";
+import {validateToken} from "../../middleware/validateToken.js";
 
 
 const router = Router();
 
-router.get('/', getAllUsers);
-router.get('/:userID', getUserById);
-router.delete('/:userID', deleteUserByID);
+router.get('/',getAllUsers);
+router.get('/:userID',validateToken,getUserById);
+router.delete('/:userID',validateToken,deleteUserByID);
 
 
 export default router;
